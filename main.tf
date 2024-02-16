57b4265224817c79ab085cf32cd396cfaa76ff3e
prudhvi <jamithireddyprudhvi9395@gmail.com>
2024-02-17 00:27:38 +0530

    edit

--- main.tf
+++ main.tf
@@ -156,19 +156,19 @@ resource "aws_route" "database_route" {
 }
 
 resource "aws_route_table_association" "public" {
-  count = length(var.public_subnet_cidr)
+  count = length(var.public_subnets_cidr)
   subnet_id = element(aws_subnet.public[*].id, count.index)   
   route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "private" {
-  count = length(var.private_subnet_cidr)
+  count = length(var.private_subnets_cidr)
   subnet_id = element(aws_subnet.private[*].id, count.index)   
   route_table_id = aws_route_table.private.id
 }
 
 resource "aws_route_table_association" "database" {
-  count = length(var.database_subnet_cidr)
+  count = length(var.database_subnets_cidr)
   subnet_id = element(aws_subnet.database[*].id, count.index)   
   route_table_id = aws_route_table.database.id
 }
